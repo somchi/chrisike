9f6ac0afdd1d877018c31a8926dd5b339f3c9bae
somchi <chisomike11@gmail.com>
2023-11-21 14:31:37 +0100

    update banner image

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import App from '../App';
 import AdminHome from '../pages/admin';
 import AddDesign from '../pages/admin/design';
